6f7c26e07ad4f3092976ad3c3ad008ee3b1e6442
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2025-08-08 19:38:59 +0300

    navbar-style

---
+++ app/Navbar.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React from 'react'
+import Link from 'next/link'
+import { AiFillBug } from 'react-icons/ai'
+import { link } from 'fs'
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames';
+
+function Navbar() {
+
+    const currentPath = usePathname()
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
+    ]
+
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+        <Link href={"/"}> <AiFillBug/> </Link>
+        <ul className='flex space-x-6'>
+            {links.map(link => 
+                <Link  
+                    key={link.href} 
+                    className={classNames({
+                        'text-zinc-900': link.href === currentPath,
+                        'text-zinc-500 hover:text-zinc-800 transition-colors': link.href !== currentPath
+                    })}
+                    href={link.href}> {link.label} 
+                </Link>)}
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar

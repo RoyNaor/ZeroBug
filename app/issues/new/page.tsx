08268e7a19e2fe873beefb3515f36a843812007b
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2025-08-09 14:29:31 +0300

    form new issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,46 @@
 'use client';
 
-import { TextField, Button, Card, TextArea } from '@radix-ui/themes';
-import React from 'react';
+import { TextField, Button, Card } from '@radix-ui/themes';
+import dynamic from 'next/dynamic';
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from 'react-hook-form';
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
+
+interface IssueForm {
+  title: string;    
+  description: string;
+}
 
 function NewIssuePage() {
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>();
+
   return (
     <div className="flex items-center justify-center min-h-[60vh] bg-gray-50">
       <Card className="w-full max-w-md p-8 shadow-lg border border-gray-200">
         <h2 className="text-2xl font-bold mb-6 text-center">Create New Issue</h2>
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit((data) => console.log('data:', data))}>
           <TextField.Root placeholder="Title" className="w-full">
-            <TextField.Slot  />
+            <TextField.Slot {...register('title')} />
           </TextField.Root>
-            <TextArea placeholder="Description" />
+          <Controller 
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE
+                {...field}
+                options={{
+                  placeholder: "Description",
+                  spellChecker: false,
+                  autosave: {
+                    enabled: true,
+                    uniqueId: "issue-description",
+                    delay: 1000,
+                  },
+                }}
+              />
+            )}
+          />
           <Button type="submit" className="w-full">Submit</Button>
         </form>
       </Card>
